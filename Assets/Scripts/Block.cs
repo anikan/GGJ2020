4a0532176aadfe52d2e950a1fc4b0d695d62f222
Charlie Su <dragonsmirth@gmail.com>
2020-02-01 15:54:53 -0800

    Added RemoveBlock and cleaned up TestBlockScript
    * RemoveBlock does not divide the grid into connected components
    * The TestBlockScript now deletes game objects that fails to be added to
    the block
    * Block now extends Interactable

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Block : MonoBehaviour
+public class Block : Interactable
 {
     [SerializeField] private int hp;
     public bool isSafeToAttachTo = true;
     public float mass = 1;
+
+    public override void OnUse(Player player)
+    {
+        // TO FILL
+    }
 }
--- Assets/Scripts/BlockManager.cs
+++ Assets/Scripts/BlockManager.cs
@@ -4,16 +4,14 @@ using UnityEngine;
 
 public class BlockManager : MonoBehaviour
 {
-    // The grid that holds the blocks
-    private Dictionary<Vector2Int, Block> grid;
+    private Dictionary<Vector2Int, Block> grid; // the grid that holds the blocks
+    private Vector2 blockDimension = Vector2.one; // dimension of the blocks (assuming 1x1)
 
-    // Dimension of the blocks
-    private Vector2 blockDimension = Vector2.one;
-
-    // Add every prefabricated block
     public void Awake()
     {
         grid = new Dictionary<Vector2Int, Block>();
+
+        // If there are already children blocks in the BlockManager object, add them
         var blocks = GetComponentsInChildren<Block>();
         foreach(Block block in blocks)
         {
@@ -21,10 +19,18 @@ public class BlockManager : MonoBehaviour
         }
     }
 
+    // Converts World Position to the internal grid position
+    private Vector2Int GetGridIndex(Vector2 worldPosition)
+    {
+        Vector2 gridPosition = worldPosition - new Vector2(this.transform.position.x, this.transform.position.y);
+        gridPosition = new Vector2(gridPosition.x / blockDimension.x, gridPosition.y / blockDimension.y);
+        return new Vector2Int((int)gridPosition.x, (int)gridPosition.y);
+    }
+
     // Checks the north, south, west, and east locations for a neighbor
     private bool CanPlaceBlockHere(Vector2Int index)
     {
-        // if it's empty, check if it has neighbors we can attach to
+        // If it's empty, check if it has neighbors we can attach to
         if(!grid.ContainsKey(index))
         {
             Vector2Int north = new Vector2Int(index.x, index.y + 1);
@@ -46,26 +52,20 @@ public class BlockManager : MonoBehaviour
     // Forcefully adds a block without any checks
     private bool SudoAddBlock(Block block, Vector2 worldPosition)
     {
-        // Convert the world position to the local grid index
-        Vector2 gridPosition = worldPosition - new Vector2(this.transform.position.x, this.transform.position.y);
-        gridPosition = new Vector2(gridPosition.x / blockDimension.x, gridPosition.y / blockDimension.y);
-        Vector2Int gridIndex = new Vector2Int((int)gridPosition.x, (int)gridPosition.y);
-
-        // Add it to the grid
+        Vector2Int gridIndex = GetGridIndex(worldPosition);
         grid[gridIndex] = block;
         block.transform.position = new Vector3(gridIndex.x, gridIndex.y, 0);
         block.transform.SetParent(this.transform);
         return true;
     }
+
+    // Adds a block according to the World Position
     public bool AddBlock(Block block, Vector2 worldPosition)
     {
-        // Convert the world position to the local grid index
-        Vector2 gridPosition = worldPosition - new Vector2(this.transform.position.x, this.transform.position.y);
-        gridPosition = new Vector2(gridPosition.x / blockDimension.x, gridPosition.y / blockDimension.y);
-        Vector2Int gridIndex = new Vector2Int((int)gridPosition.x, (int)gridPosition.y);
+        Vector2Int gridIndex = GetGridIndex(worldPosition);
 
         // Check if there is an undamaged neighbor that we can attach to
-        if(CanPlaceBlockHere(gridIndex))
+        if (CanPlaceBlockHere(gridIndex))
         {
             // Add it to the grid
             grid[gridIndex] = block;
@@ -76,4 +76,31 @@ public class BlockManager : MonoBehaviour
         }
         return false;
     }
+
+    // Removes the block at the world position, if a valid block is there
+    // Returns null if there is no block there
+    // NOTE: This will run a check and also disconnect all blocks!
+    // How to determine which one is part of the ship: which side the user is standing on
+    public Block RemoveBlock(Vector2 worldPosition, Vector3 playerPosition)
+    {
+        Vector2Int gridIndex = GetGridIndex(worldPosition);
+        if (!grid.ContainsKey(gridIndex)) return null;
+
+        Block selectedBlock = grid[gridIndex];
+        RemoveDisconnectedBlocks(gridIndex, playerPosition);
+        return selectedBlock;
+    }
+
+    // A block is considered disconnected if from the block the player is getting to
+    // to the current block, it is unable to access it once the current block has been
+    // removed.
+    // I'm too lazy to read up on decremental connectivity of graphs, so I'll do this sketchily:
+    // (1) Run BFS to find all connected components
+    // (2) Toss the connected components that the player is not on
+    private void RemoveDisconnectedBlocks(Vector2Int gridIndex, Vector3 playerPosition)
+    {
+        // Convert player position to a grid index
+        // TO DO
+    }
+
 }

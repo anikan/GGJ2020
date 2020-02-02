56c018ae9fa6c060163afb45d4585e443e2b44d5
Connor Smith <connor@connorasmith.com>
2020-02-01 20:50:58 -0800

    added player zooming

--- Assets/Scripts/TopDownLazyFollow.cs
+++ Assets/Scripts/TopDownLazyFollow.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class TopDownLazyFollow : MonoBehaviour
 {
-    public Transform objectToFollow;
+    public Transform boatTransform;
+    public Transform playerTransform;
+    public float playerViewYOffset = -10.0f;
+    public float steeringViewYOffset = -25.0f;
+
     public float smoothTime;
-    public float yOffset = -20f;
+
     private Vector3 velocity;
+
+    public bool isFollowingPlayer = true;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +24,18 @@ public class TopDownLazyFollow : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 targetPosition = objectToFollow.transform.position + (new Vector3(0, 0, yOffset));
+        Transform objectToFollow = isFollowingPlayer ? playerTransform : boatTransform;
+        Vector3 targetPosition = objectToFollow.transform.position + (new Vector3(0, 0, isFollowingPlayer? playerViewYOffset : steeringViewYOffset));
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+
+    public void ZoomToPlayerView()
+    {
+        isFollowingPlayer = true;
+    }
+
+    public void ZoomToBoatView()
+    {
+        isFollowingPlayer = false;
     }
 }

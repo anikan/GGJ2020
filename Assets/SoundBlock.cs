49ed69e4203debf61762c38b1bc60a4b80b76d0c
Connor Smith <connor@connorasmith.com>
2020-02-02 19:02:03 -0800

    Fancy new sound block

---
+++ Assets/SoundBlock.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundBlock : Block
+{
+    public AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnUse(Player player)
+    {
+        base.OnUse(player);
+
+        if (source.isPlaying)
+        {
+            source.Stop();
+        }
+        else
+        {
+            source.Play();
+        }
+    }
+}
